a48ec9b2ab2775d2c4ddc3fd1f79607b343081ac
Wilhelm Leikanger <wilhelm.leikanger@student.nhh.no>
2024-09-26 14:48:09 +0200

    Add implementation of argmax()

--- Argmax-example.py
+++ Argmax-example.py
@@ -7,7 +7,7 @@ Implement and test argmax() function that returns the location of a maximum.
 
 
 def argmax(values):
-    """
+    '''
     Return the location and value of the maximum contained in a given sequence.
 
     Parameters
@@ -16,14 +16,23 @@ def argmax(values):
 
     Returns
     -------
+    
     imax : int
         Location of the maximum
     vmax : int or float
         Maximum value
-    """
+    '''
 
     # ADD YOUR IMPLEMENTATION HERE
+    imax=0
+    vmax=values[0]
+    for i in range(len(values)):
+        if values[i]>vmax:
+            imax=i
+            vmax=values[i]
+    return imax, vmax
 
+    
 
 def main():
 
@@ -31,9 +40,9 @@ def main():
     values = [2, 3, -1, 7, 4]
 
     # Use argmax() to locale the maximum
-
+    maximum=argmax(values)
     # ADD YOUR IMPLEMENTATION HERE
-
+    print(maximum)
 
 if __name__ == '__main__':
     main()
